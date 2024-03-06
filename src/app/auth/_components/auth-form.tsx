9d4927f00203b5c27e0d7b953a53519014729860
Pvvirg <pvvirg16@gmail.com>
2024-03-06 19:55:38 -0300

    micro-saas project

---
+++ src/app/auth/_components/auth-form.tsx
@@ -0,0 +1,43 @@
+"use client";
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
+export function AuthForm() {
+  const form = useForm();
+  const handleSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <Card className=" mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="m@example.com"
+              required
+              type="email"
+            />
+          </div>
+          <Button className="w-full">Send Magic Link</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
